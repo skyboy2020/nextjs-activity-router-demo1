e747cfe7e9ac17b9b7a4333104dd86f842003f13
skyboy2020 <89569970@qq.com>
2022-03-23 11:09:50 +0800

    add components/*

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,8 @@
-import Link from 'next/link'
+// import Link from 'next/link'
+import DateIcon from '../icons/date-icon'
+import AddressIcon from '../icons/address-icon'
+import ArrowRightIcon from '../icons/arrow-right-icon'
+import Button from '../ui/button'
 import classes from './event-item.module.css'
 
 function EventItem(props) {
@@ -21,14 +25,22 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDAte}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{location}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>查看活动详情</Link>
+          {/* <Link href={exploreLink}>查看活动详情</Link> */}
+          <Button link={exploreLink}>
+            <span>查看活动详情</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
